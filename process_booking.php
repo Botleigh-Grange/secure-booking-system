2244f669cdb786898c667fe558e85ac6143ccf18
Botleigh-Grange <151997230+Botleigh-Grange@users.noreply.github.com>
2023-12-16 17:58:26 +0000

    Update process_booking.php

--- process_booking.php
+++ process_booking.php
@@ -1,1 +1,19 @@
+<?php
+// Perform server-side validation and processing here
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$date = $_POST['date'];
+
+// Validate and sanitize data if needed
+
+// Save data to a secure location (e.g., database)
+
+// Send a confirmation email to the user or perform other necessary actions
+
+// Redirect the user to a thank you page
+header("Location: thank_you.html");
+exit();
+?>
 
